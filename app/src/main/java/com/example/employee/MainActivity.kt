fef4c792bcde70fd018b8bac912ebce25cf1e2f1
NHIPP <NHIPP@nh.ctg>
2019-08-25 20:49:23 +0600

    First commit

--- app/src/main/java/com/example/employee/MainActivity.kt
+++ app/src/main/java/com/example/employee/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.employee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-
+    private val TAG = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"We are in our pause Method")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"We are in our Destroy Method")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"We are in our Resume Method")
+    }
+
+
 }
